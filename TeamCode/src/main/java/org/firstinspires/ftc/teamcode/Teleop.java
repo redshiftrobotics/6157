aa59bda256e2a25e09dcd25fbf396ca9735649f6
Sam Bouchey <sambouchey@seattleacademy.org>
2018-11-15 18:40:23 -0800

    Fixed TeleOp to work with new chassis
    commented lines with arm/intake motors
    
    added reverse function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Eclipse Teleop")
 public class Teleop extends LinearOpMode {
@@ -10,12 +11,12 @@ public class Teleop extends LinearOpMode {
     DcMotor leftDrive;
     DcMotor rightDrive;
 
-    DcMotor armLiftBottomA;
-    DcMotor armLiftBottomB;
-    DcMotor armLiftTopA;
-    DcMotor armLiftTopB;
-
-    DcMotor intakeMotor;
+//    DcMotor armLiftBottomA;
+//    DcMotor armLiftBottomB;
+//    DcMotor armLiftTopA;
+//    DcMotor armLiftTopB;
+//
+//    DcMotor intakeMotor;
 
     @Override
     public void runOpMode() {
@@ -25,12 +26,14 @@ public class Teleop extends LinearOpMode {
         this.leftDrive = hardwareMap.dcMotor.get("left_drive");
         this.rightDrive = hardwareMap.dcMotor.get("right_drive");
 
-        this.armLiftBottomA = hardwareMap.dcMotor.get("bottom_a");
-        this.armLiftBottomB = hardwareMap.dcMotor.get("bottom_b");
-        this.armLiftTopA = hardwareMap.dcMotor.get("top_a");
-        this.armLiftTopB = hardwareMap.dcMotor.get("top_b");
+        this.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        this.intakeMotor = hardwareMap.dcMotor.get("intake");
+//        this.armLiftBottomA = hardwareMap.dcMotor.get("bottom_a");
+//        this.armLiftBottomB = hardwareMap.dcMotor.get("bottom_b");
+//        this.armLiftTopA = hardwareMap.dcMotor.get("top_a");
+//        this.armLiftTopB = hardwareMap.dcMotor.get("top_b");
+//
+//        this.intakeMotor = hardwareMap.dcMotor.get("intake");
 
         telemetry.addLine("Done.");
         telemetry.update();
@@ -41,12 +44,12 @@ public class Teleop extends LinearOpMode {
             this.leftDrive.setPower(gamepad1.left_stick_y);
             this.rightDrive.setPower(gamepad1.right_stick_y);
 
-            this.armLiftBottomA.setPower(gamepad2.left_stick_y);
-            this.armLiftBottomB.setPower(gamepad2.left_stick_y);
-            this.armLiftTopA.setPower(gamepad2.right_stick_y);
-            this.armLiftTopB.setPower(gamepad2.right_stick_y);
-
-            this.intakeMotor.setPower(gamepad2.left_trigger);
+//            this.armLiftBottomA.setPower(gamepad2.left_stick_y);
+//            this.armLiftBottomB.setPower(gamepad2.left_stick_y);
+//            this.armLiftTopA.setPower(gamepad2.right_stick_y);
+//            this.armLiftTopB.setPower(gamepad2.right_stick_y);
+//
+//            this.intakeMotor.setPower(gamepad2.left_trigger);
 
             idle();
         }
