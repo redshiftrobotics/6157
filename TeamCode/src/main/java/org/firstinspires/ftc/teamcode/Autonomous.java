0d7e27c8726c3fc2516f47e8f9442a5c32fe3876
Sam Bouchey <sambouchey@seattleacademy.org>
2018-11-20 13:10:42 -0800

    Basic Auto Tweaks
    Removed initialization and hardwere maping of unneaded motors.
    Added comment answers to some questions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,24 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@TeleOp(name = "Autonomous")
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = Autonomous)
 public class Autonomous extends LinearOpMode {
 
     DcMotor leftDrive;
     DcMotor rightDrive;
 
-    DcMotor armLiftBottomA;
-    DcMotor armLiftBottomB;
-    DcMotor armLiftTopA;
-    DcMotor armLiftTopB;
+//    DcMotor armLiftBottomA;
+//    DcMotor armLiftBottomB;
+//    DcMotor armLiftTopA;
+//    DcMotor armLiftTopB;
+//
+//    DcMotor intakeMotor;
 
-    DcMotor intakeMotor;
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
@@ -29,14 +30,16 @@ public class Autonomous extends LinearOpMode {
         this.leftDrive = hardwareMap.dcMotor.get("left_drive");
         this.rightDrive = hardwareMap.dcMotor.get("right_drive");
         //Q: is this important? (below)
+        //Otherwise the drive controls for the joysticks are fucked.
         this.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        this.armLiftBottomA = hardwareMap.dcMotor.get("bottom_a");
-        this.armLiftBottomB = hardwareMap.dcMotor.get("bottom_b");
-        this.armLiftTopA = hardwareMap.dcMotor.get("top_a");
-        this.armLiftTopB = hardwareMap.dcMotor.get("top_b");
-
-        this.intakeMotor = hardwareMap.dcMotor.get("intake");
+//        this.armLiftBottomA = hardwareMap.dcMotor.get("bottom_a");
+//        this.armLiftBottomB = hardwareMap.dcMotor.get("bottom_b");
+//        this.armLiftTopA = hardwareMap.dcMotor.get("top_a");
+//        this.armLiftTopB = hardwareMap.dcMotor.get("top_b");
+//
+//        this.intakeMotor = hardwareMap.dcMotor.get("intake");
+//          We don't have these on our bot anymore
 
         telemetry.addLine("Done.");
         telemetry.update();
@@ -44,6 +47,8 @@ public class Autonomous extends LinearOpMode {
 
         while (getRuntime()< 3) {
             //makes robot move, but how does it stop? Need:rotation count
+            // This could be *Encoders* (A piece of hardware that you put on your wheels that can
+            //help you control for things like battery power etc. OR PID... --- Alek is looking into this
 
             this.leftDrive.setPower(.5);
             this.rightDrive.setPower(.5);
