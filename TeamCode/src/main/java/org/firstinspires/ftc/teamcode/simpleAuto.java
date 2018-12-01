b9942aef9b6e54c8fb0dcb4012fff8242c762fae
Alek Curless <alekcurless@seattleacademy.org>
2018-12-01 15:31:03 -0800

    ontoesntuhnteoh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleAuto.java
@@ -1,19 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@Autonomous
+@Autonomous(name = "Eclipse Auto")
 public class simpleAuto extends LinearOpMode {
 
     DcMotor leftDrive;
     DcMotor rightDrive;
-    DcMotor lightOfTheHevens;
+    DcMotor hevenlyLight;
+
+
 
     @Override
     public void runOpMode() {
-        this.leftDrive =
+        telemetry.addLine("Initializing...");
+        telemetry.update();
+
+        this.leftDrive = hardwareMap.dcMotor.get("left_drive");
+        this.rightDrive = hardwareMap.dcMotor.get("right_drive");
+        this.hevenlyLight = hardwareMap.dcMotor.get("lift_drive");
+
+        this.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        telemetry.addLine("Done.");
+        telemetry.update();
+
+        
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            this.leftDrive.setPower(gamepad1.left_stick_y);
+            this.rightDrive.setPower(gamepad1.right_stick_y);
+
+
+            idle();
+        }
     }
 }
