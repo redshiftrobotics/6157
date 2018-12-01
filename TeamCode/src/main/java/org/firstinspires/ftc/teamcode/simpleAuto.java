8e589d1e7834c3dcd2d812d490d61c45a51533d3
Sam Bouchey <sambouchey@seattleacademy.org>
2018-12-01 15:21:21 -0800

    Create simpleAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleAuto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class simpleAuto extends LinearOpMode {
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    DcMotor lightOfTheHevens;
+
+    @Override
+    public void runOpMode() {
+        this.leftDrive =
+    }
+}
